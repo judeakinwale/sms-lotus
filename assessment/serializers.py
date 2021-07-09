921f9466489e6e92f24b68d829a273c03a2becf0
judeakinwale <judeakinwale@gmail.com>
2021-07-09 22:46:32 +0100

    Improve assessment serializers and endpoints

--- assessment/serializers.py
+++ assessment/serializers.py
@@ -3,35 +3,81 @@ from assessment import models
 
 
 class QuizSerializer(serializers.HyperlinkedModelSerializer):
+    """serializer for the Quiz model"""
+    supervisor = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = models.Quiz
         fields = '__all__'
+        extra_kwargs = {
+            'url': {'view_name': 'assessment:quiz-detail'}
+        }
 
 
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    """serializer for the Question model"""
+    quiz = serializers.HyperlinkedRelatedField(
+        queryset=models.Quiz.objects.all(),
+        view_name='information:quiz-detail',
+    )
 
     class Meta:
         model = models.Question
         fields = '__all__'
+        extra_kwargs = {
+            'url': {'view_name': 'assessment:question-detail'}
+        }
 
 
 class AnswerSerializer(serializers.HyperlinkedModelSerializer):
+    """serializer for the Answer model"""
+    question = serializers.HyperlinkedRelatedField(
+        queryset=models.Question.objects.all(),
+        view_name='information:question-detail',
+    )
 
     class Meta:
         model = models.Answer
         fields = '__all__'
+        extra_kwargs = {
+            'url': {'view_name': 'assessment:answers-detail'}
+        }
 
 
 class QuizTakerSerializer(serializers.HyperlinkedModelSerializer):
+    """serializer for the QuizTaker model"""
+    student = serializers.PrimaryKeyRelatedField(read_only=True)
+    quiz = serializers.HyperlinkedRelatedField(
+        queryset=models.Quiz.objects.all(),
+        view_name='information:quiz-detail',
+    )
 
     class Meta:
         model = models.QuizTaker
         fields = '__all__'
+        extra_kwargs = {
+            'url': {'view_name': 'assessment:quiztaker-detail'}
+        }
 
 
 class ResponseSerializer(serializers.HyperlinkedModelSerializer):
+    """serializer for the Response model"""
+    quiz_taker = serializers.HyperlinkedRelatedField(
+        queryset=models.QuizTaker.objects.all(),
+        view_name='information:quiz_taker-detail',
+    )
+    question = serializers.HyperlinkedRelatedField(
+        queryset=models.Question.objects.all(),
+        view_name='information:question-detail',
+    )
+    answer = serializers.HyperlinkedRelatedField(
+        queryset=models.Answer.objects.all(),
+        view_name='information:answer-detail',
+    )
 
     class Meta:
         model = models.Response
         fields = '__all__'
+        extra_kwargs = {
+            'url': {'view_name': 'assessment:response-detail'}
+        }
--- assessment/views.py
+++ assessment/views.py
@@ -13,23 +13,23 @@ class QuizViewSet(viewsets.ModelViewSet):
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = models.Question.objects.all()
-    serializer_class = serializers.QuizSerializer
+    serializer_class = serializers.QuestionSerializer
     permission_classes = [permissions.IsAuthenticated | permissions.IsAdminUser]
 
 
 class AnswerViewSet(viewsets.ModelViewSet):
     queryset = models.Answer.objects.all()
-    serializer_class = serializers.QuizSerializer
+    serializer_class = serializers.AnswerSerializer
     permission_classes = [permissions.IsAuthenticated | permissions.IsAdminUser]
 
 
 class QuizTakerViewSet(viewsets.ModelViewSet):
     queryset = models.QuizTaker.objects.all()
-    serializer_class = serializers.QuizSerializer
+    serializer_class = serializers.QuizTakerSerializer
     permission_classes = [permissions.IsAuthenticated | permissions.IsAdminUser]
 
 
 class ResponseViewSet(viewsets.ModelViewSet):
     queryset = models.Response.objects.all()
-    serializer_class = serializers.QuizSerializer
+    serializer_class = serializers.ResponseSerializer
     permission_classes = [permissions.IsAuthenticated | permissions.IsAdminUser]
